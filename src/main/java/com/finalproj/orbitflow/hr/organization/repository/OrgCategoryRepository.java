6c384e3ee197ae443978ce97a19b9e892105c0e8
seunga03 <seungalee220@gmail.com>
2025-12-16 14:11:56 +0900

    feat(org, company): entity 및 repository 구현

---
+++ src/main/java/com/finalproj/orbitflow/hr/organization/repository/OrgCategoryRepository.java
@@ -0,0 +1,27 @@
+package com.finalproj.orbitflow.hr.organization.repository;
+
+import com.finalproj.orbitflow.hr.organization.entity.OrgCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : seunga03
+ * @filename : OrgCategoryRepository
+ * @since : 2025-12-16 화요일
+ */
+public interface OrgCategoryRepository extends JpaRepository<OrgCategory, Long> {
+
+    /**
+     * 회사별 조직 카테고리 전체 조회 (정렬 포함)
+     */
+    List<OrgCategory> findByCompanyIdAndIsActiveTrueOrderByOrderIndexAsc(Long companyId);
+
+    /**
+     * 회사별 특정 조직 카테고리 조회
+     */
+    Optional<OrgCategory> findByCompanyIdAndCategoryId(Long companyId, Long categoryId);
+}
