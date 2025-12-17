b047512c72b858010ffa439e3aac7631313c8a00
Jonghun Baek <gnswhd0415@gmail.com>
2025-12-17 15:13:34 +0900

    feat(car) : 관리자 차량 CRUD

---
+++ src/main/java/com/finalproj/orbitflow/resource/itemcategory/controller/ItemCategoryController.java
@@ -0,0 +1,20 @@
+package com.finalproj.orbitflow.resource.itemcategory.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 종훈
+ * @filename : ItemCategoryController
+ * @since : 2025-12-17 오후 2:51 수요일
+ */
+@RequestMapping("/api")
+@Controller
+@RequiredArgsConstructor
+public class ItemCategoryController {
+
+    
+}
