b9834a9c0357a439990590475daa75b35f27a691
seunga03 <seungalee220@gmail.com>
2025-12-18 11:09:24 +0900

    feat(global): 공통 커스텀 예외 및 GlobalExceptionHandler 추가 + SecurityUtils 수정

---
+++ src/main/java/com/finalproj/orbitflow/global/exception/ForbiddenException.java
@@ -0,0 +1,14 @@
+package com.finalproj.orbitflow.global.exception;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : seunga03
+ * @filename : ForbiddenException
+ * @since : 2025-12-18 목요일
+ */
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/finalproj/orbitflow/global/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.finalproj.orbitflow.global.exception;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : seunga03
+ * @filename : NotFoundException
+ * @since : 2025-12-18 목요일
+ */
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
