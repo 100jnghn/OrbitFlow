48a973970ed49005756398f8eaf848ac0ac9f845
Choi Min Hyeok <chlals12267@gmail.com>
2025-12-18 14:50:37 +0900

    fix(AffectTag): JSON 형태의 AffectTag를 ENUM으로 관리 및 DB에도 대문자로 추가되도록 변경 및 로직 내에서 JSON과 String으로의 convert 코드 구현

---
+++ src/main/java/com/finalproj/orbitflow/approval/formTemplate/enums/AffectTag.java
@@ -0,0 +1,15 @@
+package com.finalproj.orbitflow.approval.formTemplate.enums;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : Choi MinHyeok
+ * @filename : AffectTag
+ * @since : 25. 12. 18. 목요일
+ **/
+
+
+public enum AffectTag {
+    ATTENDANCE,
+    SCHEDULE
+}
