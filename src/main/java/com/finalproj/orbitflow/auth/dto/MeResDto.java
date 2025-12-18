ef620e9f156761424058487ac49e80d3512bb6f8
seunga03 <seungalee220@gmail.com>
2025-12-18 21:06:08 +0900

    feat(auth): JWT 인증/인가 인프라 및 ROLE 기반 접근 제어 구현

--- src/main/java/com/finalproj/orbitflow/auth/dto/MeResDto.java
+++ src/main/java/com/finalproj/orbitflow/auth/dto/MeResDto.java
@@ -7,13 +7,14 @@ import lombok.Getter;
  * Please explain the class!!!
  *
  * @author : seunga03
- * @filename : LoginResponse
- * @since : 2025-12-16 화요일
+ * @filename : MeResDto
+ * @since : 2025-12-18 목요일
  */
 @Getter
 @AllArgsConstructor
-public class LoginResDto {
-
-    private String accessToken;
-    private String refreshToken;
+public class MeResDto {
+    private Long employeeId;
+    private String name;
+    private String email;
+    private String role;
 }
