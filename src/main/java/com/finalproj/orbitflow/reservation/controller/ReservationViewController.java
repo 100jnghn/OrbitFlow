ecd40ad78008c20ede7d8386a850d9cea21cb894
Jonghun Baek <gnswhd0415@gmail.com>
2025-12-23 15:33:18 +0900

    feat(reserevation): 회의실 예약
    - 예약 페이지

--- src/main/java/com/finalproj/orbitflow/reservation/controller/ReservationViewController.java
+++ src/main/java/com/finalproj/orbitflow/reservation/controller/ReservationViewController.java
@@ -1,11 +1,12 @@
 package com.finalproj.orbitflow.reservation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Please explain the class!!!
+ * 예약 View Controller
  *
  * @author : 종훈
  * @filename : ReservationViewController
@@ -15,9 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReservationViewController {
 
+    /**
+     * 내 예약 현황 페이지
+     */
     @GetMapping("/me")
-    public String getMyReservationPage() {
+    public String getMyReservationPage(Model model) {
+        model.addAttribute("pageTitle", "내 예약 현황");
+        model.addAttribute("currentGNB", "reservation");
+        model.addAttribute("currentMenu", "my-reservation");
         return "reservation/my-reservation";
+    }
+
+    /**
+     * 회의실 예약 페이지
+     */
+    @GetMapping("/meetingroom")
+    public String getMeetingReservationPage(Model model) {
+        model.addAttribute("pageTitle", "회의실 예약");
+        model.addAttribute("currentGNB", "reservation");
+        model.addAttribute("currentMenu", "meeting-reservation");
+        return "reservation/meetingroom-reservation";
     }
 
 }
