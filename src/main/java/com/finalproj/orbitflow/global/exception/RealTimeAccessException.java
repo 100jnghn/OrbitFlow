729fc732619a344a382bef9bd6cae3e1c6d4d0f6
Jonghun Baek <gnswhd0415@gmail.com>
2026-01-16 13:08:11 +0900

    design(reservation): 셀 hover 디자인 수정

---
+++ src/main/java/com/finalproj/orbitflow/global/exception/RealTimeAccessException.java
@@ -0,0 +1,14 @@
+package com.finalproj.orbitflow.global.exception;
+
+/**
+ * Please explain the class!!!
+ *
+ * @author : 종훈
+ * @filename : RealTimeAccessException
+ * @since : 2026-01-16 오전 10:57 금요일
+ */
+public class RealTimeAccessException extends RuntimeException {
+    public RealTimeAccessException(String message) {
+        super(message);
+    }
+}
