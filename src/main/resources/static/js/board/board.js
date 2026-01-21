df19d53f78cb4328b1c0c4c6b2ef4d813633de19
Jeon Sung Gu <jsg307@nate.com>
2026-01-21 13:07:06 +0900

    fix(board, dashboard): 불필요 주석 삭제

--- src/main/resources/static/js/board/board.js
+++ src/main/resources/static/js/board/board.js
@@ -421,10 +421,6 @@ function renderPagination(page) {
     const pagination = document.getElementById('boardPagination');
     pagination.innerHTML = '';
 
-    // 페이징 버튼 상시 출력
-    // if (totalPages <= 1) {
-    //     return;
-    // }
 
     // 이전 버튼
     const prevBtn = document.createElement('button');
